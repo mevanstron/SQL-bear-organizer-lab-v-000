a1674ffd3b33da89f840718ae61b68037f5cb964
Michael Evans <michael@michaelgregoryevans.com>
2017-10-26 19:26:15 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,10 +1,8 @@
-("Mr. Chocolate", age, gender, color, temper, alive)
-
-INSERT INTO bears ("Mr. Chocolate", 3, "M", "black", "nice", 1)
-INSERT INTO bears ("Rowdy", 5, "M", "brown", "angry", 1)
-INSERT INTO bears ("Tabitha", 10, "F", "black", "angry", 1)
-INSERT INTO bears ("Sergeant Brown", 3, "M", "white", "nice", 1)
-INSERT INTO bears ("Melissa", 1, "M", "black", "angry", 1)
-INSERT INTO bears ("Grinch", 3, "F", "white", "nice", 1)
-INSERT INTO bears ("Wendy", 5, "F", "black", "neutral", 1)
-INSERT INTO bears (NULL, 7, "M", "brown", "neutral", 0)
+INSERT INTO bears ("Mr. Chocolate", 3, "M", "black", "nice", 1);
+INSERT INTO bears ("Rowdy", 5, "M", "brown", "angry", 1);
+INSERT INTO bears ("Tabitha", 10, "F", "black", "angry", 1);
+INSERT INTO bears ("Sergeant Brown", 3, "M", "white", "nice", 1);
+INSERT INTO bears ("Melissa", 1, "M", "black", "angry", 1);
+INSERT INTO bears ("Grinch", 3, "F", "white", "nice", 1);
+INSERT INTO bears ("Wendy", 5, "F", "black", "neutral", 1);
+INSERT INTO bears (NULL, 7, "M", "brown", "neutral", 0);
