5afb32431a21eaf6236d14213fd8e1d98ec2d017
Michael Evans <michael@michaelgregoryevans.com>
2017-10-26 19:27:39 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears ("Mr. Chocolate", 3, "M", "black", "nice", 1);
-INSERT INTO bears ("Rowdy", 5, "M", "brown", "angry", 1);
-INSERT INTO bears ("Tabitha", 10, "F", "black", "angry", 1);
-INSERT INTO bears ("Sergeant Brown", 3, "M", "white", "nice", 1);
-INSERT INTO bears ("Melissa", 1, "M", "black", "angry", 1);
-INSERT INTO bears ("Grinch", 3, "F", "white", "nice", 1);
-INSERT INTO bears ("Wendy", 5, "F", "black", "neutral", 1);
-INSERT INTO bears (NULL, 7, "M", "brown", "neutral", 0);
+INSERT INTO bears VALUES ("Mr. Chocolate", 3, "M", "black", "nice", 1);
+INSERT INTO bears VALUES ("Rowdy", 5, "M", "brown", "angry", 1);
+INSERT INTO bears VALUES ("Tabitha", 10, "F", "black", "angry", 1);
+INSERT INTO bears VALUES ("Sergeant Brown", 3, "M", "white", "nice", 1);
+INSERT INTO bears VALUES ("Melissa", 1, "M", "black", "angry", 1);
+INSERT INTO bears VALUES ("Grinch", 3, "F", "white", "nice", 1);
+INSERT INTO bears VALUES ("Wendy", 5, "F", "black", "neutral", 1);
+INSERT INTO bears VALUES (NULL, 7, "M", "brown", "neutral", 0);
